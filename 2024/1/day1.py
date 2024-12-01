385473aadbe2f0d4559277400c1677c2f62bb1ec
Lucas Gracia <146884567+LuG-r@users.noreply.github.com>
2024-12-01 18:18:16 +0000

    day1 python solution added

---
+++ 2024/1/day1.py
@@ -0,0 +1,37 @@
+import typing
+from collections import Counter
+
+def part1(input: typing.TextIO) -> int:
+    answer = 0
+    lista, listb = [], []
+    for line in input:
+        line = line.strip()
+        a, b = line.split("   ")
+        lista.append(int(a))
+        listb.append(int(b))
+
+    for a,b in zip(sorted(lista), sorted(listb)):
+        answer += abs(a - b)
+
+    return answer
+
+def part2(input: typing.TextIO) -> int:
+    answer = 0
+    lista, listb = [], []
+    for line in input:
+        line = line.strip()
+        a, b = line.split("   ")
+        lista.append(int(a))
+        listb.append(int(b))
+
+    counter = Counter(listb)
+    for num in lista:
+        answer += num * counter[num]
+
+    return answer
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as infile:
+        print(part1(infile))
+        infile.seek(0)
+        print(part2(infile))
